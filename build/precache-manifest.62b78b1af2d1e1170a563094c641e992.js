d2c1ba834ea8da6d599c03cb5f72777d4df261c1
AndresU04 <123709630+AndresU04@users.noreply.github.com>
2025-05-09 13:15:04 -0700

    Add files via upload

---
+++ build/precache-manifest.62b78b1af2d1e1170a563094c641e992.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344d8a0fe551a91758ea024188278844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2421366bbd9f66d8aff1",
+    "url": "/static/css/main.b5ea8900.chunk.css"
+  },
+  {
+    "revision": "d95bb8f1f0aef44715c1",
+    "url": "/static/js/2.748294ad.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/static/js/2.748294ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef90e41bcf13bdafb7b",
+    "url": "/static/js/3.82afb7a3.chunk.js"
+  },
+  {
+    "revision": "2421366bbd9f66d8aff1",
+    "url": "/static/js/main.5a876b71.chunk.js"
+  },
+  {
+    "revision": "6521385c839cadf3457c",
+    "url": "/static/js/runtime-main.5ef167bd.js"
+  }
+]);
